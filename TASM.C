e1a09a7b8b7658378b17391435c24073b8860276
neofenixlive <neofenixlive@gmail.com>
2026-01-07 16:20:14 -0300

    Add files via upload

--- TASM.C
+++ TASM.C
@@ -7,12 +7,13 @@
 #define FLAG_N 0x4
 
 #define TASM_CheckOpr(s1, s2) ((s1)[0]==(s2)[0] && (s1)[1]==(s2)[1] && (s1)[2]==(s2)[2])
-#define TASM_SetZN(m, v) do {        \
+
+#define TASM_SetZN(m, v) do {             \
     if ((v) == 0) { (m)->P |= FLAG_Z; }   \
     else { (m)->P &= ~FLAG_Z; }           \
     if ((v) & 0x80) { (m)->P |= FLAG_N; } \
     else { (m)->P &= ~FLAG_N; } } while(0)
-#define TASM_SetC(m, v1, v2) do {    \
+#define TASM_SetC(m, v1, v2) do {         \
     if((v1) >= (v2)) { (m)->P |= FLAG_C;} \
     else { (m)->P &= ~FLAG_C; } } while(0)
 
@@ -21,7 +22,8 @@ enum TASM_INSTRUCTIONS {
     LDA, LDX, LDY, STA, STX, STY, TAX, TAY, TSX, TXA, TYA, TXS, /* memory */
     PHP, PLP, PHA, PLA, CLC, SEC,                               /* stack and flags */
     DEC, DEX, DEY, INC, INX, INY, ADC, SBC, ASL, LSR, ROL, ROR, /* arithmetic */
-    CMP, CPX, CPY, BEQ, BNE, BMI, BPL, JMP, JSR, RTS            /* control flow */
+    CMP, CPX, CPY, BEQ, BNE, BMI, BPL, JMP, JSR, RTS,           /* control flow */
+    END
 };
 
 enum TASM_ADDRESS {
@@ -33,16 +35,17 @@ enum TASM_ADDRESS {
     IDY  /* index Y */
 };
 
+/* assembly enviroment */
 struct TASM_Machine {
-    unsigned int** ROM; unsigned int PC;               /* program */
+    unsigned char* ROM; unsigned int PC;               /* program */
     unsigned char* RAM; unsigned char SP;              /* memory */
     unsigned char A; unsigned char X; unsigned char Y; /* registers */
-    unsigned char P;                                   /* flags */
+    unsigned int P;                                    /* flags */
 };
 
 /* parses string into token */
-void* TASM_Parser(char* S, struct TASM_Machine* M) {
-    int* T = malloc(sizeof(int) * 3);
+void* TASM_Parser(char* S) {
+    unsigned int* T = calloc(4, sizeof(int));
     int Idx = 0;
     int SubIdx = 0;
     int IsHex = 0;
@@ -52,14 +55,9 @@ void* TASM_Parser(char* S, struct TASM_Machine* M) {
     char* Line = NULL;
     void* Temp = NULL;
 
-    T[0] = 0;
-    T[1] = 0;
-    T[2] = 0;
-
+    /* removes whitespace and ignores comments */
     for (Idx = 0; S[Idx] != '\0'; Idx++) {
-        if (S[Idx] == ' ' || S[Idx] == '\t' || S[Idx] == '\n') {
-            continue;
-        }
+        if (S[Idx] == ' ' || S[Idx] == '\t' || S[Idx] == '\n') { continue; }
         if (S[Idx] == ';') { break; }
 
         Temp = realloc(Line, sizeof(char) * (SubIdx + 2));
@@ -78,37 +76,38 @@ void* TASM_Parser(char* S, struct TASM_Machine* M) {
     
     if (SubIdx < 3) { free(Line); return T; }
 
-    if (TASM_CheckOpr(Line, "NOP")) { T[0] = NOP; T[2] = IMP; }
+    /* checks operation */
+    if (TASM_CheckOpr(Line, "NOP")) { T[0] = NOP; T[1] = IMP; }
     else if (TASM_CheckOpr(Line, "LDA")) { T[0] = LDA; }
     else if (TASM_CheckOpr(Line, "LDX")) { T[0] = LDX; }
     else if (TASM_CheckOpr(Line, "LDY")) { T[0] = LDY; }
     else if (TASM_CheckOpr(Line, "STA")) { T[0] = STA; }
     else if (TASM_CheckOpr(Line, "STX")) { T[0] = STX; }
     else if (TASM_CheckOpr(Line, "STY")) { T[0] = STY; }
-    else if (TASM_CheckOpr(Line, "TAX")) { T[0] = TAX; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "TAY")) { T[0] = TAY; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "TSX")) { T[0] = TSX; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "TXA")) { T[0] = TXA; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "TYA")) { T[0] = TYA; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "TXS")) { T[0] = TXS; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "PHP")) { T[0] = PHP; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "PLP")) { T[0] = PLP; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "PHA")) { T[0] = PHA; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "PLA")) { T[0] = PLA; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "CLC")) { T[0] = CLC; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "SEC")) { T[0] = SEC; T[2] = IMP; }
+    else if (TASM_CheckOpr(Line, "TAX")) { T[0] = TAX; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "TAY")) { T[0] = TAY; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "TSX")) { T[0] = TSX; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "TXA")) { T[0] = TXA; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "TYA")) { T[0] = TYA; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "TXS")) { T[0] = TXS; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "PHP")) { T[0] = PHP; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "PLP")) { T[0] = PLP; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "PHA")) { T[0] = PHA; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "PLA")) { T[0] = PLA; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "CLC")) { T[0] = CLC; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "SEC")) { T[0] = SEC; T[1] = IMP; }
     else if (TASM_CheckOpr(Line, "DEC")) { T[0] = DEC; }
-    else if (TASM_CheckOpr(Line, "DEX")) { T[0] = DEX; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "DEY")) { T[0] = DEY; T[2] = IMP; }
+    else if (TASM_CheckOpr(Line, "DEX")) { T[0] = DEX; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "DEY")) { T[0] = DEY; T[1] = IMP; }
     else if (TASM_CheckOpr(Line, "INC")) { T[0] = INC; }
-    else if (TASM_CheckOpr(Line, "INX")) { T[0] = INX; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "INY")) { T[0] = INY; T[2] = IMP; }
+    else if (TASM_CheckOpr(Line, "INX")) { T[0] = INX; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "INY")) { T[0] = INY; T[1] = IMP; }
     else if (TASM_CheckOpr(Line, "ADC")) { T[0] = ADC; }
     else if (TASM_CheckOpr(Line, "SBC")) { T[0] = SBC; }
-    else if (TASM_CheckOpr(Line, "ASL")) { T[0] = ASL; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "LSR")) { T[0] = LSR; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "ROL")) { T[0] = ROL; T[2] = IMP; }
-    else if (TASM_CheckOpr(Line, "ROR")) { T[0] = ROR; T[2] = IMP; }
+    else if (TASM_CheckOpr(Line, "ASL")) { T[0] = ASL; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "LSR")) { T[0] = LSR; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "ROL")) { T[0] = ROL; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "ROR")) { T[0] = ROR; T[1] = IMP; }
     else if (TASM_CheckOpr(Line, "CMP")) { T[0] = CMP; }
     else if (TASM_CheckOpr(Line, "CPX")) { T[0] = CPX; }
     else if (TASM_CheckOpr(Line, "CPY")) { T[0] = CPY; }
@@ -118,60 +117,65 @@ void* TASM_Parser(char* S, struct TASM_Machine* M) {
     else if (TASM_CheckOpr(Line, "BPL")) { T[0] = BPL; SaveAs16 = 1; }
     else if (TASM_CheckOpr(Line, "JMP")) { T[0] = JMP; SaveAs16 = 1; }
     else if (TASM_CheckOpr(Line, "JSR")) { T[0] = JSR; SaveAs16 = 1; }
-    else if (TASM_CheckOpr(Line, "RTS")) { T[0] = RTS; T[2] = IMP; }
+    else if (TASM_CheckOpr(Line, "RTS")) { T[0] = RTS; T[1] = IMP; }
+    else if (TASM_CheckOpr(Line, "END")) { T[0] = END; T[1] = IMP; }
 
-    if (T[2] == IMP) { free(Line); return T; }
-
+    if (T[1] == IMP) { free(Line); return T; }
     Idx = 3;
-    if (Line[Idx] == '#') { T[2] = IMM; Idx++; }
-    else { T[2] = ABS; }
+    
+    /* writes data from string */
+    if (Line[Idx] == '#') { T[1] = IMM; Idx++; }
+    else { T[1] = ABS; }
     if (Line[Idx] == '$') { IsHex = 1; Idx++; }
     else if (Line[Idx] == '%') { IsBin = 1; Idx++; }
 
     while (Line[Idx] != '\0') {
-        if (T[2] == ABS && Line[Idx] == ',') {
-            if (Line[Idx + 1] == 'X') { T[2] = IDX; break; }
-            else if (Line[Idx + 1] == 'Y') { T[2] = IDY; break; }
+        if (T[1] == ABS && Line[Idx] == ',') {
+            if (Line[Idx + 1] == 'X') { T[1] = IDX; break; }
+            else if (Line[Idx + 1] == 'Y') { T[1] = IDY; break; }
         }
         
         if (IsHex) {
-            T[1] *= 16;
+            T[2] *= 16;
             if (Line[Idx] >= '0' && Line[Idx] <= '9') {
-                T[1] += Line[Idx] - '0';
+                T[2] += Line[Idx] - '0';
             }
             else if (Line[Idx] >= 'A' && Line[Idx] <= 'F') {
-                T[1] += Line[Idx] - 'A' + 10;
+                T[2] += Line[Idx] - 'A' + 10;
             }
         }
         else if (IsBin && (Line[Idx] == '0' || Line[Idx] == '1')) {
-            T[1] *= 2;
-            T[1] += Line[Idx] - '0';
+            T[2] *= 2;
+            T[2] += Line[Idx] - '0';
         }
         else if (Line[Idx] >= '0' && Line[Idx] <= '9') {
-            T[1] *= 10;
-            T[1] += Line[Idx] - '0';
+            T[2] *= 10;
+            T[2] += Line[Idx] - '0';
         }
         
         Idx++;
     }
+    if (SaveAs16) { T[3] = (T[2] >> 8) & 0xFF00; }
+    T[2] &= 0xFF;
     
-    if (!SaveAs16 && T[2] == IMM) { T[1] &= 0xFF; }
     free(Line);
     return T;
 }
 
 /* evaluates current token */
 void TASM_Eval(struct TASM_Machine* M) {
-    int Value = 0;
-    int* Data = &Value;
+    int Value = (M->ROM[M->PC+3] << 8) | M->ROM[M->PC+2];
+    int* Data = NULL;
     int LastC = M->P & FLAG_C;
-    
-    if (M->ROM[M->PC][2] == IMM) { *Data = M->ROM[M->PC][1]; }
-    else if (M->ROM[M->PC][2] == ABS) { Data = (unsigned int*)&M->RAM[M->ROM[M->PC][1]]; }
-    else if (M->ROM[M->PC][2] == IDX) { Data = (unsigned int*)&M->RAM[M->ROM[M->PC][1] + M->X]; }
-    else if (M->ROM[M->PC][2] == IDY) { Data = (unsigned int*)&M->RAM[M->ROM[M->PC][1] + M->Y]; }
+       
+    /* data for address modes */
+    if (M->ROM[M->PC+1] == IMM) { Data = &Value; }
+    else if (M->ROM[M->PC+1] == ABS) { Data = (unsigned int*)&M->RAM[Value]; }
+    else if (M->ROM[M->PC+1] == IDX) { Data = (unsigned int*)&M->RAM[Value + M->X]; }
+    else if (M->ROM[M->PC+1] == IDY) { Data = (unsigned int*)&M->RAM[Value + M->Y]; }
 
-    switch (M->ROM[M->PC][0]) {
+    /* operation execution */
+    switch (M->ROM[M->PC]) {
     default: break;
     case LDA: M->A = *Data; break;
     case LDX: M->X = *Data; break;
@@ -278,22 +282,21 @@ void TASM_Eval(struct TASM_Machine* M) {
         break;
     }
     
-    M->PC++;
+    M->PC += 4;
 }
 
 /* creates a new machine */
 void* TASM_Start(char* F) {
     struct TASM_Machine* M = malloc(sizeof(struct TASM_Machine));
-
     FILE* Program = fopen(F, "r");
     char* Instruction = malloc(sizeof(char) * 21);
-
     int Idx = 0;
-    void* Temp = NULL;
+    
     if (!Program) { printf("--<File missing!>--\n"); exit(1); }
 
-    M->ROM = calloc(1, sizeof(int*));
-    M->RAM = calloc(0x10000, sizeof(char));
+    /* setup new machine */
+    M->ROM = calloc(0x4000, sizeof(char));
+    M->RAM = calloc(0x2000, sizeof(char));
     M->PC = 0x0000;
     M->SP = 0xFF;
     M->A = 0;
@@ -301,25 +304,15 @@ void* TASM_Start(char* F) {
     M->Y = 0;
     M->P = 0;
 
+    /* write operations */
     while (fgets(Instruction, 21, Program)) {
-        int* T = TASM_Parser(Instruction, M);
-
-        Temp = realloc(M->ROM, sizeof(int*) * (Idx + 2));
-        if (Temp == NULL) {
-            free(T);
-            free(Instruction);
-            fclose(Program);
-            free(M->ROM);
-            free(M->RAM);
-            free(M);
-            printf("--<Read-only memory failed!>--\n");
-            exit(1);
-        }
-        M->ROM = Temp;
-
-        M->ROM[Idx] = T;
-        M->ROM[Idx + 1] = NULL;
-        Idx++;
+        unsigned int* T = TASM_Parser(Instruction);
+        M->ROM[Idx] = (char)T[0];
+        M->ROM[Idx + 1] = (char)T[1];
+        M->ROM[Idx + 2] = (char)T[2];
+        M->ROM[Idx + 3] = (char)T[3];
+        M->ROM[Idx + 4] = END;
+        Idx += 4;
     }
 
     free(Instruction);
@@ -333,22 +326,20 @@ void TASM_Execute(struct TASM_Machine* M, char* Name) {
     int SubIdx;
     clock_t Start;
 
-    printf("\033[2J");
-    while (M->ROM[M->PC] != NULL) {
+    /* prints information and delays by 0.1 seconds */
+    while (M->ROM[M->PC] != END) {
         TASM_Eval(M);
         SubIdx = 0;
         Start = clock();
         
-        printf("\033[H");
-        
+        printf("\033[2J");
         printf("--<CPU>----------------------\n");
         printf("\"%s\"\n", Name);
         printf("(PC)$%04X, (SP)$%04X\n", M->PC, M->SP);
         printf("(A)$%02X, (X)$%02X, (Y)$%02X\n", M->A, M->X, M->Y);
         printf("(C)%d (N)%d (Z)%d\n", ((M->P & FLAG_C) != 0), ((M->P & FLAG_N) != 0), ((M->P & FLAG_Z) != 0));
-        
         printf("--<RAM>----------------------\n");
-        for (Idx = 0; Idx < 0xFFFF; Idx++) {
+        for (Idx = 0; Idx < 0x2000; Idx++) {
             if (M->RAM[Idx] != 0) { printf("($%04X)$%02X ", Idx, M->RAM[Idx]); SubIdx++; }
             if (SubIdx == 4) { putchar('\n'); SubIdx = 0; }
         }
